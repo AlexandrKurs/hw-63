5554393b300ac955f235d9f245085298123052c9
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-10-26 02:51:40 +0600

    Add components and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-const App = () => {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
+import Home from "./components/Home/Home.tsx";
+import About from "./components/About/About.tsx";
+import Contacts from "./components/Contacts/Contacts.tsx";
+import Add from "./components/Add/Add.tsx";
+import "./Styles.css";
 
+const App: React.FC = () => {
   return (
-    <>
-      Hello!
-    </>
-  )
-
+    <Router>
+      <div className="app info-container">
+        <nav>
+          <p className="title-name">My Blog</p>
+          <div className="menu">
+            <NavLink to="/" end>
+              Home
+            </NavLink>
+            <NavLink to="/add">Add</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contacts />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
-export default App; 
+
+export default App;
---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contacts: React.FC = () => {
+  return (
+    <div>
+      <h2 className="title">Contacts</h2>
+    </div>
+  );
+};
+
+export default Contacts;
